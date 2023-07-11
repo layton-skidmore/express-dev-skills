fff468d7d3f35cc05992cd99730ef3a74178becd
layton-skidmore <layton.skidmore@gmail.com>
2023-07-11 13:49:12 -0500

    home link

--- routes/skills.js
+++ routes/skills.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-// Require the controller that exports To-Do CRUD functions
+
 var skillsCtrl = require('../controllers/skills');
 
-// All actual paths begin with "/todos"
 
-// GET /todos
 router.get('/', skillsCtrl.index);
 
 router.get('/:id', skillsCtrl.show);
